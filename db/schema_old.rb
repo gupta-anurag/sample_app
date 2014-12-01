915bee0be871575a429914ae7c9a201e4c5a655c
Anurag Gupta <anurag19gupta@gmail.com>
2014-12-01 21:13:34 +0530

    Some fixes

--- db/schema_old.rb
+++ db/schema_old.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141201133109) do
+ActiveRecord::Schema.define(version: 20141130055351) do
+
+  create_table "homepages", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.text     "message"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "microposts", force: true do |t|
     t.text     "content"
