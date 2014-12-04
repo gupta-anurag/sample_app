b06fe186cb6e6f932751850410c8d38c55f04a10
Anurag Gupta <anurag19gupta@gmail.com>
2014-12-04 14:10:55 +0530

    heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,43 +24,16 @@ class UsersController < ApplicationController
   end
   
   def update
-
   end
 
   def destroy
-
   end
 
-  private
+private
 
-   def user_params
+  def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-   end
-  end  
-  
-  def show
-    @user = User.find(params[:id]) 
   end
-
-  def new
-  	@user = User.new
-  end
-
-  def create
-  	@user = User.new(user_params)
-  	if @user.save 
-      log_in @user
-      flash[:success] = "welcome to the Sample App!"
-      redirect_to @user
-  	else
-  	  render 'new'
-  	end
-  end   	
-
-  private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation) 
-    end  
+ end  
 end
 
