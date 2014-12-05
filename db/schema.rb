a496da8e24ffac6ce88f389672c1841692648809
Anurag Gupta <anurag19gupta@gmail.com>
2014-12-05 17:10:25 +0530

    Add passwordd resets @ email configuration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141205073357) do
+ActiveRecord::Schema.define(version: 20141205091946) do
 
   create_table "microposts", force: true do |t|
     t.text     "content"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20141205073357) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
