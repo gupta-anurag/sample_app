5cff1dac808c33982f4abdf2feed390190e6adbe
Anurag Gupta <anurag19gupta@gmail.com>
2014-12-05 11:29:11 +0530

    syntax error fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url 
-
+  end
 private
 
   def user_params
