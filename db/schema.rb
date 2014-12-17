f5eae9cee1d4e856a1f20e62e5945ac2e2e5d124
Anurag Gupta <anurag19gupta@gmail.com>
2014-12-17 17:55:45 +0530

    Changes

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,12 @@
 ActiveRecord::Schema.define(version: 20141216134012) do
 
   create_table "comments", force: true do |t|
-    t.string   "commenter"
-    t.text     "body"
+    t.string   "content"
+    t.integer  "user_id"
     t.integer  "micropost_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "comments", ["micropost_id"], name: "index_comments_on_micropost_id"
 
   create_table "microposts", force: true do |t|
     t.text     "content"
