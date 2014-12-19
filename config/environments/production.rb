b7fc8edcf422207b643773e746b1d3f59265417e
Anurag Gupta <anurag19gupta@gmail.com>
2014-12-19 11:38:07 +0530

    Finish that

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,19 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_mailer.default_url_options = { :host => 'afternoon-refuge-6806.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "heroku.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "rubyrails19@gmail.com",
+    password: "anurag19#"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
