62fc3a9f8f722e7bdc2361d7cd7d72c90909ec9c
Anurag Gupta <anurag19gupta@gmail.com>
2014-12-19 17:47:22 +0530

    Changes and Finish

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "herokuapp.com",
+    domain: "heroku.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "rubyonrails19@gmail.com",
