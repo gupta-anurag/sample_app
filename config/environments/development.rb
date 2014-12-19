821aec32f48cb5ec9611f440b4bb7ff5bc341cce
Anurag Gupta <anurag19gupta@gmail.com>
2014-12-19 14:45:59 +0530

    Finish this demo app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,11 +16,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-<<<<<<< HEAD
+
   host = 'http://localhost:3000'
-=======
-  host = 'localhost:3000'
->>>>>>> user-comments
+
   config.action_mailer.default_url_options = { host: host } 
 
   # Print deprecation notices to the Rails logger.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,19 +73,19 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'afternoon-refuge-6806.herokuapp.come' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "herokuapp.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: "rubyrails19@gmail.com"
-    password: "anurag1991#"
+  # config.action_mailer.default_url_options = { :host => 'afternoon-refuge-6806.herokuapp.come' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.default :charset => "utf-8"
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: "herokuapp.com",
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: "rubyrails19@gmail.com"
+  #   password: "anurag1991#"
 
   # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.delivery_method = :smtp
