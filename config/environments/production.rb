cf39369577e186f0df0dcf515ae3c7dd8a373ea4
Anurag Gupta <anurag19gupta@gmail.com>
2014-12-19 11:41:32 +0530

    Finish that

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
     enable_starttls_auto: true,
     user_name: "rubyrails19@gmail.com",
     password: "anurag19#"
-
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
